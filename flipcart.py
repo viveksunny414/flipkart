be7d96d3c994f1b76c63c37e19e41c6b9a5bdfb5
viveksunny414 <110971498+viveksunny414@users.noreply.github.com>
2022-08-10 14:29:32 +0530

    Update flipcart.py

--- flipcart.py
+++ flipcart.py
@@ -1,7 +1,7 @@
 class login:
     def __init__(self,id,pas):
-        self.id="admin"
-        self.pas="admin"
+        self.id="adminn"
+        self.pas="adminn"
 
     def check(id,pas):
         print self.id
